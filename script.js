2adfec2ed86bb6d43011e1827505a536dc32c24a
marcelkrzywy33-cloud <marcel.krzywy33@gmail.com>
2026-05-22 22:14:02 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+window.onload = function () {
+
+  const input = document.getElementById("input");
+  const output = document.getElementById("output");
+
+  const SECRET = "database access dossier 68235";
+
+  function typeText(text, speed = 25) {
+    let i = 0;
+
+    const line = document.createElement("div");
+    output.appendChild(line);
+
+    const interval = setInterval(() => {
+      line.innerHTML += text[i];
+      i++;
+
+      if (i >= text.length) {
+        clearInterval(interval);
+      }
+    }, speed);
+  }
+
+  function instant(text) {
+    output.innerHTML += text + "\n";
+  }
+
+  // startup text
+  typeText("TERMINAL ONLINE", 30);
+  typeText("ENTER COMMAND", 30);
+
+  input.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+
+      const value = input.value.trim().toLowerCase();
+
+      instant("> " + value);
+
+      if (value === SECRET) {
+        typeText("ACCESS GRANTED", 40);
+        typeText("LOADING DOSSIER 68235...", 40);
+        typeText("████████████████████", 40);
+      } else {
+        typeText("ACCESS DENIED", 40);
+      }
+
+      input.value = "";
+    }
+  });
+
+};
